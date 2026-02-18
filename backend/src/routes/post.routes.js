b4e1fb9b206f4b722f00de52eee7704c18b31b0f
roshan nawkar <roshannawkar2807@gmail.com>
2026-02-18 20:08:34 +0530

    add auth middleware

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -3,6 +3,7 @@ const postRouter = express.Router();
 const postController = require("../controllers/post.controller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
+const identifyUser = require("../middlewares/auth.middleware.js");
 
 /**
  * POST /api/posts [protected]
@@ -11,7 +12,17 @@ const upload = multer({ storage: multer.memoryStorage() });
 postRouter.post(
   "/",
   upload.single("image"),
+  identifyUser,
   postController.createPostController,
 );
 
+//get Post Rout
+postRouter.get("/", identifyUser, postController.getPostController);
+
+//return an detail about specific post
+postRouter.get(
+  "/details/:postId",
+  identifyUser,
+  postController.getPostDetailsController,
+);
 module.exports = postRouter;
